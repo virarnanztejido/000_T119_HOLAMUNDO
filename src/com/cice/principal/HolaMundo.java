d886888e4d1dc0927e7443992bc807b4f402a894
Vir <Vir@192.168.1.38>
2017-06-13 22:28:12 +0200

    (INITIAL COMMIT) Envío todos los archivos con este comentario en el que detallaré los cambios realizados.

---
+++ src/com/cice/principal/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cice.principal;
+
+/**
+ *
+ * @author Vir
+ */
+public class HolaMundo {
+    
+    //variable local de la clase
+    private static final String SALUDO = "Hola Mundo";
+    
+    public static void main(String[] args) {
+        
+        System.out.println(SALUDO);
+    
+}
+    
+}
